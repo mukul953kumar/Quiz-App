8e7aed0346e35ab8ccce9875d301f50e67aed311
kushgupta5258a <kushgupta5258a@gmail.com>
2025-11-24 02:48:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,54 @@
+# The "API Quiz App" Debugging Challenge
+
+Welcome! Your job is to fix a buggy and incomplete Quiz Application.
+A previous developer left this project partially implemented. There are multiple UI issues, logic errors, and missing features.
+
+Your mission is to:
+
+1. Identify and fix the bugs.
+2. Add the missing functionalities.
+
+---
+
+# HOW TO START
+
+1. Open the project folder.
+2. The app contains:
+
+    `index.html`
+    `style.css`
+    `script.js`
+
+3. Open `index.html` in any browser to run the quiz.
+4. Internet connection is required because the quiz uses an external API.
+
+---
+
+# WHAT YOU HAVE (EXISTING CODE)
+
+###  Basic Quiz Logic
+
+* Fetches questions from Open Trivia DB API
+* Displays one question at a time
+* Shows correct/incorrect feedback
+* Tracks score
+
+---
+# PROJECT STRUCTURE
+
+/quiz-app
+│── index.html
+│── style.css
+│── script.js
+│── README.md
+│── issues.md
+
+---
+
+# YOUR GOAL
+
+Fix the issues and enhance the project with missing features.
+Make the quiz fully functional, responsive, and user‑friendly.
+
+Good luck debugging!
+Happy Coding ^_^
---
+++ script.js
@@ -0,0 +1,89 @@
+let questions = [];
+let index = 0;
+let score = 0;
+
+const questionEl = document.getElementById("question");
+const optionsEl = document.getElementById("options");
+const scoreBox = document.getElementById("scoreBox");
+const newGameBtn = document.getElementById("newGame");
+
+function fetchQuestions() {
+    questionEl.textContent = "Loading questions...";
+    optionsEl.innerHTML = "";
+    scoreBox.textContent = "";
+
+    fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
+    .then(res => res.json())
+    .then(data => {
+        questions = data.results.map(q => formatQuestion(q));
+        index = 0;
+        score = 0;
+    })
+    .catch(() => {
+        questionEl.textContent = "Failed to load questions. Try again.";
+    });
+}
+
+function formatQuestion(q) {
+    const options = [...q.incorrect_answers];
+    const correctPosition = Math.floor(Math.random() * 5);
+    options.splice(correctPosition, 0, q.correct_answer);
+
+    return {
+    question: q.question,
+    options: options,
+    answer: correctPosition
+    };
+}
+
+function loadQuestion() {
+    if (index >= questions.length) return endQuiz();
+
+    const q = questions[index];
+    questionEl.innerHTML = q.question;
+    optionsEl.innerHTML = "";
+
+    q.options.forEach((opt, i) => {
+        const div = document.createElement("div");
+        div.className = "option";
+        div.innerHTML = opt;
+
+        div.onclick = () => checkAnswer(i, div);
+        optionsEl.appendChild(div);
+    });
+}
+
+function checkAnswer(selected, selectedDiv) {
+    const correct = questions[index].answer;
+
+    Array.from(optionsEl.children).forEach(opt => opt.style.pointerEvents = "none");
+
+    if (selected === correct) {
+        selectedDiv.style.background = "#4caf50";
+        score++;
+    } 
+    else {
+        selectedDiv.style.background = "#f44336";
+        optionsEl.children[correct].style.background = "#4caf50";
+    }
+
+    setTimeout(() => {
+        index++;
+        loadQuestion();
+    }, 900);
+}
+
+function endQuiz() {
+    questionEl.innerHTML = score >= 3 ? "<center>🎉 Congratulations! You Won!</center" : "<center>You finished the quiz!</center";
+
+    optionsEl.innerHTML = "";
+    scoreBox.innerHTML = `Your Score: <strong>${score} / ${questions.length}</strong>`;
+    newGameBtn.style.display = "block";
+}
+
+newGameBtn.onclick = () => {
+    newGameBtn.style.display = "none";
+    fetchQuestions();
+};
+
+fetchQuestions();
